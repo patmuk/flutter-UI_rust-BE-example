efe846c00e69982991de1158df1e71460e6ce9d6
Patrick Mukherjee <patrick@mukherjee.de>
2023-03-25 21:24:07 +0100

    fixes layout

--- shell_flutter/lib/main.dart
+++ shell_flutter/lib/main.dart
@@ -39,111 +39,96 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   MainScreen({super.key, required this.title});
 
   final String title;
 
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
   // this triggers the button event in the rust lib
   void _addTodo(String todo) {
     api.processEvent(event: Event_AddTodo(todo));
-    setState() {}
+    textController.clear();
+    // triggers a refresh
+    setState(() {});
   }
 
   final TextEditingController textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(title),
+        title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "---=== TODO LIST ===---",
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            FutureBuilder(
-              future: api.view(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  final viewModel = snapshot.data;
-                  if (viewModel!.count == 0) {
-                    return const Text(
-                      "No Todo's in the List!",
+      body: Column(
+        children: [
+          Text(
+            "---=== TODO LIST ===---",
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Expanded(
+            child: Column(verticalDirection: VerticalDirection.up, children: [
+              FutureBuilder(
+                future: api.view(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final viewModel = snapshot.data;
+                    if (viewModel!.count == 0) {
+                      return const Center(
+                        child: Text("No Todo's in the List!"),
+                      );
+                    } else {
+                      return Expanded(
+                        child: ListView.builder(
+                          scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          itemCount: viewModel.count,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              title: Text(
+                                '${index + 1}. todo: ${viewModel.items[index]}',
+                              ),
+                            );
+                          },
+                        ),
+                      );
+                    }
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text('Error: ${snapshot.error}'),
                     );
                   } else {
-                    return ListView.builder(
-                      scrollDirection: Axis.vertical,
-                      shrinkWrap: true,
-                      itemCount: viewModel.count,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: Text(
-                              '${index+1}. todo: ${viewModel.items[index]}'),
-                        );
-                      },
+                    return const Center(
+                      child: CircularProgressIndicator(),
                     );
-
-//                     return Text(
-//                       '${viewModel.items}',
-// //                    'From Rust: ${data?.items.first}',
-//                     );
                   }
-                } else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                } else {
-                  return const CircularProgressIndicator();
-                }
-              },
-            ),
-            Column(
-              children: [
-                TextField(
-                  controller: textController,
-                  maxLines: null,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    _addTodo(textController.text);
-                  },
-                  child: const Text("Add Todo"),
-                ),
-              ],
-            ),
-          ],
-        ),
+                },
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: textController,
+                      maxLines: null,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      _addTodo(textController.text);
+                    },
+                    child: const Text("Add Todo"),
+                  ),
+                ],
+              ),
+            ]),
+          ),
+        ],
       ),
     );
   }
