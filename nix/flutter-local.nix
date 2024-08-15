21334737968fb731631c564039ea3aedd3837ecc
patmuk <patrick@mukherjee.de>
2024-08-15 14:53:32 +0200

    shifts version setting logic from bash to nix

--- nix/flutter-local.nix
+++ nix/flutter-local.nix
@@ -8,6 +8,10 @@
 # leave `hash = ""` and run `nix develop`. The error message will tell the correct hash value.
 rec {
   latest_version = "3.24.0";
+  desired_version = if (flutter_version == null || flutter_version == "latest") then latest_version else flutter_version;
+  desired_version_ = builtins.replaceStrings [ "." ] [ "_" ] desired_version;
+
+  flutter_source = "flutterSource-aarch64-darwin-${desired_version_}";
 
   flutterSource-aarch64-darwin-3_24_0 = pkgs.fetchurl {
     url = "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_3.24.0-stable.zip";
@@ -22,7 +26,6 @@ rec {
     hash = "sha256-TmEUDfSWUr+PweMXUqb6hRj6TwW8bkxrywHI3/bZv48=";
   };
   flutterSource-aarch64-darwin-3_19_5 = pkgs.fetchurl {
-    # url = "https://github.com/flutter/flutter/archive/refs/tags/3.19.5.zip"
     url = "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_arm64_3.19.5-stable.zip";
     hash = "sha256-HXHsHs2bzt9Xaqp6cUyK/S/Qk028jqCfSx3DF31HX/Q=";
   };
@@ -40,40 +43,15 @@ rec {
     text = ''
       flutter_bin_dir="${local_flutter_path}"/flutter/bin
       flutter_bin_file="$flutter_bin_dir"/flutter
-      desired_version=${flutter_version}
-      if [ $desired_version = "latest" ]; then
-        desired_version="${latest_version}"
-      fi
-      case $desired_version in
-        "3.24.0")
-          flutter_source="${flutterSource-aarch64-darwin-3_24_0}"
-        ;;
-        "3.22.2")
-          flutter_source="${flutterSource-aarch64-darwin-3_22_2}"
-        ;;
-        "3.19.6")
-          flutter_source="${flutterSource-aarch64-darwin-3_19_6}"
-        ;;
-        "3.19.5")
-          flutter_source="${flutterSource-aarch64-darwin-3_19_5}"
-        ;;
-        "3.19.4")
-          flutter_source="${flutterSource-aarch64-darwin-3_19_4}"
-        ;;
-        *)
-          echo "no source for version ${flutter_version} found, using latest version (${latest_version}) instead..."
-          flutter_source="${flutterSource-aarch64-darwin-3_19_6}"
-        ;;
-      esac
-
+ 
       echo "flutter needs local installation? ..."
       if [ -f "$flutter_bin_file" ]; then
         local_flutter_version=$( $flutter_bin_file --version | grep -oP 'Flutter \K.*(?= • channel)')
-        if [ "$local_flutter_version" = "$desired_version" ]; then
+        if [ "$local_flutter_version" = "${desired_version}" ]; then
           echo "flutter $local_flutter_version is already installed locally in '${local_flutter_path}'"
           install=false
         else
-          echo "flutter is already installed locally, but the installed version '$local_flutter_version' is not the same as requested version '$desired_version'.  Uninstalling..."
+          echo "flutter is already installed locally, but the installed version '$local_flutter_version' is not the same as requested version '${desired_version}'.  Uninstalling..."
           rm -rf "${local_flutter_path}"
           install=true
         fi
@@ -81,9 +59,9 @@ rec {
         install=true
       fi
       if [ "$install" = "true" ]; then
-        echo "... installing flutter version '$desired_version' locally in '${local_flutter_path}'"
-        unzip "$flutter_source" -d "${local_flutter_path}"
-        echo "installed flutter version '$desired_version' to '${local_flutter_path}'"
+        echo "... installing flutter version '${desired_version}' locally in '${local_flutter_path}'"
+        unzip "${flutter_source}" -d "${local_flutter_path}"
+        echo "installed flutter version '${desired_version}' to '${local_flutter_path}'"
       fi
     '';
   };
