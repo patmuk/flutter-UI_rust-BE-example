9de0515730eae3e0b40081c78349d5e1f30557da
patmuk <patrick@mukherjee.de>
2024-10-26 21:45:00 +0200

    connects to the moved proc macro

--- app_core/src/application/api/processing_macro.rs
+++ app_core/src/application/api/processing_macro.rs
@@ -5,7 +5,7 @@
 //     application::app_state::AppState,
 //     domain::todo_list::{TodoListModel, TodoListProcessingError},
 // };
-use generate_api_macros::generate_api;
+use generate_cqrs_api_macros::generate_api;
 
 generate_api!(
     // "app_core/src/domain/effects.rs",
