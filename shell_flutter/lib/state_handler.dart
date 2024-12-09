dad13ab569985cb08b4579406485eca6d0f48bdd
patmuk <patrick@mukherjee.de>
2024-12-09 22:58:29 +0100

    removes unneeded import

--- shell_flutter/lib/state_handler.dart
+++ shell_flutter/lib/state_handler.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:shell_flutter/bridge/frb_generated/application/api/lifecycle.dart';
-import 'package:shell_flutter/bridge/frb_generated/domain/todo_list.dart';
 import 'package:shell_flutter/bridge/frb_generated/frb_generated.dart';
 
 /// This singleton handles the state, and all communication with the lower layers (implemented in Rust).
