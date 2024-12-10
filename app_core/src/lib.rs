68111982f39884a2a2e4007f6a808a5aa6cca165
patmuk <patrick@mukherjee.de>
2024-12-10 10:12:06 +0100

    removes logger setup from lib_main

--- app_core/src/lib.rs
+++ app_core/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod application;
 pub mod domain;
 
-fn main() {
-    simple_logger::init_with_level(log::Level::Debug).expect("faild to init logger");
-}
+// TODO fix logger setup
+// fn main() {
+//     simple_logger::init_with_level(log::Level::Debug).expect("faild to init logger");
+// }
